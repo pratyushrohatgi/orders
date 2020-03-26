8b4a7d3a081f25163e9d15ce97619ba8edaaf293
Kunal Patel <kr391@stern.nyu.edu>
2020-03-26 17:44:36 -0400

    Changes

--- service/models.py
+++ service/models.py
@@ -24,54 +24,52 @@ class PersistentBase():
 	""" Base class added persistent methods """
  
 	def create(self):
-        """
-    	Creates a Order to the database
-        """
-        logger.info("Creating %s", self.name)
-    	self.id = None  # id must be none to generate next primary key
-        db.session.add(self)
-        db.session.commit()
+		""" 
+		Creates a Order to the database
+		"""
+		logger.info("Creating %s", self.name)
+		self.id = None  # id must be none to generate next primary key
+		db.session.add(self)
+		db.session.commit()
  
 	def save(self):
-        """
-    	Updates a Order to the database
-        """
-        logger.info("Saving %s", self.name)
-        db.session.commit()
+		""" Updates a Order to the database """
+		logger.info("Saving %s", self.name)
+		db.session.commit()
  
 	def delete(self):
-        """ Removes a Order from the data store """
-        logger.info("Deleting %s", self.name)
-        db.session.delete(self)
-        db.session.commit()
+		""" Removes a Order from the data store """
+		logger.info("Deleting %s", self.name)
+		db.session.delete(self)
+		db.session.commit()
  
 	@classmethod
 	def init_db(cls, app):
-        """ Initializes the database session """
-        logger.info("Initializing database")
-    	cls.app = app
+		""" Initializes the database session """
+		logger.info("Initializing database")
+		cls.app = app
     	# This is where we initialize SQLAlchemy from the Flask app
-        db.init_app(app)
-        app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
+		db.init_app(app)
+		app.app_context().push()
+		db.create_all()  # make our sqlalchemy tables
  
 	@classmethod
 	def all(cls):
-        """ Returns all of the records in the database """
-        logger.info("Processing all records")
-    	return cls.query.all()
+		""" Returns all of the records in the database """
+		logger.info("Processing all records")
+		return cls.query.all()
  
 	@classmethod
 	def find(cls, by_id):
-        """ Finds a record by it's ID """
-        logger.info("Processing lookup for id %s ...", by_id)
-    	return cls.query.get(by_id)
+		""" Finds a record by it's ID """
+		logger.info("Processing lookup for id %s ...", by_id)
+		return cls.query.get(by_id)
  
 	@classmethod
 	def find_or_404(cls, by_id):
-        """ Find a record by it's id """
-        logger.info("Processing lookup or 404 for id %s ...", by_id)
-    	return cls.query.get_or_404(by_id)
+		""" Find a record by it's id """
+		logger.info("Processing lookup or 404 for id %s ...", by_id)
+		return cls.query.get_or_404(by_id)
  
  
 ######################################################################
@@ -90,39 +88,39 @@ class Product(db.Model, PersistentBase):
 	product_name = db.Column(db.Integer)
  
 	def __repr__(self):
-    	return "<Product %r id=[%s] order[%s]>" % (self.product_name, self.id, self.order_id)
+		return "<Product %r id=[%s] order[%s]>" % (self.product_name, self.id, self.order_id)
  
 	def __str__(self):
-    	return "%s: %s, %s, %s, %s" % (self.product_name, self.id, self.order_id,self.quantity,self.price)
+		return "%s: %s, %s, %s, %s" % (self.product_name, self.id, self.order_id,self.quantity,self.price)
  
 	def serialize(self):
-        """ Serializes a Address into a dictionary """
-    	return {
+		""" Serializes a Address into a dictionary """
+		return {
             "id": self.id,
         	"order_id": self.order_id,
-        	"product_name ": self. product_name,
-        	"quantity ": self. quantity,
-        	"price ": self. price
+        	"product_name ": self.product_name,
+        	"quantity ": self.quantity,
+        	"price ": self.price
     	}
  
 	def deserialize(self, data):
-        """
-    	Deserializes a Address from a dictionary
+		"""
+    	Deserializes a Product from a dictionary
     	Args:
         	data (dict): A dictionary containing the resource data
-        """
-    	try:
-        	self.order_id = data["order_id"]
-        	self.product_name = data["product_name "]
-        	self.quantity = data["quantity "]
-        	self.price = data["price"]
-    	except KeyError as error:
-        	raise DataValidationError("Invalid Item: missing " + error.args[0])
-    	except TypeError as error:
-        	raise DataValidationError(
-                "Invalid Item: body of request contained" "bad or no data"
-        	)
-    	return self
+    	"""
+		try:
+			self.order_id = data["order_id"]
+			self.product_name = data["product_name"]
+			self.quantity = data["quantity"]
+			self.price = data["price"]
+		except KeyError as error:
+			raise DataValidationError("Invalid Item: missing " + error.args[0])
+		except TypeError as error:
+			raise DataValidationError(
+	            "Invalid Item: body of request contained" "bad or no data"
+	    	)
+		return self
  
  
 ######################################################################
@@ -139,51 +137,51 @@ class Order(db.Model, PersistentBase):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.Integer)
 	status = db.Column(db.Enum('Delivered', 'In Progress', 'Cancelled'))
-	Products = db.relationship(Product, backref='order', lazy=True) 
+	products = db.relationship('Product', backref='order', lazy=True) 
  
 	def __repr__(self):
-    	return "<Order %r id=[%s]>" % (self.name, self.id)
+		return "<Order %r id=[%s]>" % (self.name, self.id)
  
 	def serialize(self):
-        """ Serializes a Order into a dictionary """
-    	order = {
-            "id": self.id,
-        	"name": self. names,
-        	"status": self. status,
-        	"products": []
-    	}
-    	for address in self.addresses:
-        	order[items].append(address.serialize())
-    	return order
+		""" Serializes a Account into a dictionary """
+		order = {
+			"id": self.id,
+			"name": self.names,
+			"status": self.status,
+			"products": []
+		}
+		for address in self.addresses:
+			order[items].append(address.serialize())
+		return order
  
 	def deserialize(self, data):
-        """
-    	Deserializes a Order from a dictionary
-    	Args:
-        	data (dict): A dictionary containing the resource data
-        """
-    	try:
-        	self.name = data["name"]
-        	self.status = data["status"]
-        	# handle inner list of addresses
-        	product_list = data.get("products")
-        	for json_product in product_list:
-            	product = Product ()
-            	item.deserialize(json_product)
-            	self.item.append(product)
-    	except KeyError as error:
-        	raise DataValidationError("Invalid Order: missing " + error.args[0])
-    	except TypeError as error:
-        	raise DataValidationError(
-                "Invalid Order: body of request contained" "bad or no data"
-        	)
-    	return self
+		"""
+		Deserializes a Order from a dictionary
+		Args:
+			data (dict): A dictionary containing the resource data
+		"""
+		try:
+			self.name = data["name"]
+			self.status = data["status"]
+			# handle inner list of addresses
+			product_list = data.get("products")
+			for json_product in product_list:
+				product = Product ()
+				item.deserialize(json_product)
+				self.item.append(product)
+		except KeyError as error:
+			raise DataValidationError("Invalid Order: missing " + error.args[0])
+		except TypeError as error:
+			raise DataValidationError(
+				"Invalid Order: body of request contained" "bad or no data"
+			)
+		return self
  
 	@classmethod
 	def find_by_name(cls, name):
-        """ Returns all Orders with the given customer_id
-    	Args:
-        	name(string): the name on the Orders you want to match
-        """
-        logger.info("Processing name query for %s ...", name)
-    	return cls.query.filter(cls.name == name)
+		""" Returns all Orders with the given customer_id
+		Args:
+			name(string): the name on the Orders you want to match
+		"""
+		logger.info("Processing name query for %s ...", name)
+		return cls.query.filter(cls.name == name)
--- tests/test_service.py
+++ tests/test_service.py
@@ -11,7 +11,7 @@ from unittest.mock import MagicMock, patch
 from flask_api import status  # HTTP Status Codes
 from tests.factories import OrderFactory, ProductFactory
 from service.models import db
-from service.service import app, init_db
+from service.service import app, init_db, initialize_logging
  
 # DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
 DATABASE_URI = os.getenv(
@@ -22,82 +22,80 @@ DATABASE_URI = os.getenv(
 #  T E S T   C A S E S
 ######################################################################
 class TestYourResourceServer(TestCase):
-    """ Orders Server Tests """
- 
+	""" Orders Server Tests """
 	@classmethod
 	def setUpClass(cls):
-        """ Run once before all tests """
-        app.config['TESTING'] = True
-        app.config['DEBUG'] = False
-        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-        app.logger.setLevel(logging.CRITICAL)
-    	init_db()
+		""" Run once before all tests """
+		app.config['TESTING'] = True
+		app.config['DEBUG'] = False
+		app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+		app.logger.setLevel(logging.CRITICAL)
+		init_db()
  
 	@classmethod
 	def tearDownClass(cls):
-        """ Runs once before test suite """
-    	pass
+		""" Runs once before test suite """
+		pass
  
 	def setUp(self):
-  	  """ Runs before each test """
-        db.drop_all()  # clean up the last tests
-        db.create_all()  # create new tables
-    	self.app = app.test_client()
+		""" Runs before each test """
+		db.drop_all()  # clean up the last tests
+		db.create_all()  # create new tables
+		self.app = app.test_client()
  
 	def tearDown(self):
-        """ Runs once after each test case """
-    	db.session.remove()
-    	db.drop_all()
+		""" Runs once after each test case """
+		db.session.remove()
+		db.drop_all()
  
 ######################################################################
 #  H E L P E R   M E T H O D S
 ######################################################################
  
 	def _create_orders(self, count):
-        """ Factory method to create orders in bulk """
-    	orders = []
-    	for _ in range(count):
-        	order = OrderFactory()
-        	resp = self.app.post(
-                "/orders", json=order.serialize(), content_type="application/json"
-        	)
-            self.assertEqual(
-                resp.status_code, status.HTTP_201_CREATED, "Could not create test Order"
-        	)
-        	new_order = resp.get_json()
-        	order.id = new_order["id"]
-        	orders.append(order)
-    	return orders
+		""" Factory method to create orders in bulk """
+		orders = []
+		for _ in range(count):
+			order = OrderFactory()
+			resp = self.app.post(
+				"/orders", json=order.serialize(), content_type="application/json"
+			)
+			self.assertEqual(
+				resp.status_code, status.HTTP_201_CREATED, "Could not create test Order"
+			)
+			new_order = resp.get_json()
+			order.id = new_order["id"]
+			orders.append(order)
+		return orders
  
 ######################################################################
 #  O R D E R   T E S T   C A S E S
 ######################################################################
 
 	def test_update_order(self):
-        """ Update an existing Order """
-    	# create an Order to update
-    	test_order = OrderFactory()
-    	resp = self.app.post(
-        	"/orders",
-        	json=test_order.serialize(),
-            content_type="application/json"
-    	)
-        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
- 
-    	# update the pet
-    	new_order = resp.get_json()
-    	new_order["name"] = "Happy-Happy Joy-Joy"
-    	resp = self.app.put(
-        	"/orders/{}".format(new_order["id"]),
-        	json=new_order,
-            content_type="application/json",
-    	)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-    	updated_order = resp.get_json()
-        self.assertEqual(updated_order["name"], "Happy-Happy Joy-Joy")
- 
-	
+		""" Update an existing Order """
+		# create an Order to update
+		test_order = OrderFactory()
+		resp = self.app.post(
+	    	"/orders",
+	    	json=test_order.serialize(),
+	        content_type="application/json"
+		)
+		self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+		# update the pet
+		new_order = resp.get_json()
+		new_order["name"] = "Happy-Happy Joy-Joy"
+		resp = self.app.put(
+	    	"/orders/{}".format(new_order["id"]),
+	    	json=new_order,
+	        content_type="application/json",
+		)
+		self.assertEqual(resp.status_code, status.HTTP_200_OK)
+		updated_order = resp.get_json()
+		self.assertEqual(updated_order["name"], "Happy-Happy Joy-Joy")
+
+
 ######################################################################
 #  P R O D U C T S   T E S T   C A S E S
 ######################################################################
- 
